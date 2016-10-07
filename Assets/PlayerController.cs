8637030956f3d7d8473d890b3ca526fa0864e8d1
Michael Benda <michael.benda@zeppelinstudio.net>
2016-10-07 17:56:01 +0200

    set up unity project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    void Update() {
+        if (!isLocalPlayer) {
+            return;
+        }
+
+        var rot = -Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var move = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, 0, rot);
+        transform.Translate(move, 0, 0);
+    }
+
+    public override void OnStartLocalPlayer() {
+        foreach (Renderer rend in GetComponentsInChildren <Renderer>()) rend.material.color = Color.blue;
+    }
+}

6d5243b5ca75441d79f0b2436d4fddfe921fa7d1
Christian Østergaard Laursen <cptmack@cs.au.dk>
2016-11-09 17:32:27 +0100

    Added BulletController and HealthController

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bullet : MonoBehaviour {
+public class BulletController : MonoBehaviour {
 
-	void OnCollisionEnter()	{
+	void OnCollisionEnter2D(Collision2D collision)	{
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<HealthController> ();
+
+		if (health != null) {
+			health.TakeDamage (10);
+		}
+
 		Destroy(gameObject);
 	}
+		
 }
---
+++ Assets/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthController : MonoBehaviour {
+
+	public const int maxHealth = 100;
+	public int currentHealth = maxHealth;
+
+	public void TakeDamage(int amount)	{
+		currentHealth -= amount;
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+			Debug.Log ("Dead!");
+		}
+	}
+}
